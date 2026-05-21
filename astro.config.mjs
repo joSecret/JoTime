f0e1303e85e077a3ef0788240e54acaf3e47f659
Ion Obreja <jo@da.md>
2026-05-21 15:30:23 +0300

    fix: update base path in astro.config.mjs to include leading slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://josecret.github.io',
-  base: 'JoTime',
+  base: '/JoTime/',
 });
